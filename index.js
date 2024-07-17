ed9e895893131f24971575c5edee8126c283ef10
Max <maxhenson28@icloud.com>
2024-07-17 21:58:10 +0100

    COOL

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const axios = require("axios").default;
 const { Webhook } = require("@vermaysha/discord-webhook");
+const env = require("dotenv").config();
 const app = express();
-const zammadToken =
-  "QGU91SzZZWWq5nnuJgPRhuVBVp1PoT5VWqbpyYqroFOL5Ef6ue34M2itzZZ8ikvq";
-const mattermostToken = "yt8j9ethfiyfpk3ayf471rtzbh";
-const hook = new Webhook(
-  "https://discord.com/api/webhooks/1220853079318986812/SXbonG9vMvOyyUVFAdWrkw5NPVDx1J6m7zfHnpQUFjj_2nx3LUpBQ9YEAUD1XUGFNBqx"
-);
+const zammadToken = proces.ENV.ZAMMAD_TOKEN;
+const mattermostToken = process.ENV.MATTERMOST_TOKEN;
+const hook = new Webhook(process.ENV.DISCORD_WEBHOOK);
 
 app.post("/mattermost/openticket", (req, res) => {
   hook.setContent(req.rawHeaders);
